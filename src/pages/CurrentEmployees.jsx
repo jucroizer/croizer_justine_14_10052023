a5a0454481bc9b77bd32afe2e692560165d72ed5
jucroizer <81417866+jucroizer@users.noreply.github.com>
2023-06-14 16:55:54 +0200

    modification des donnees correspondant aux states

--- src/pages/CurrentEmployees.jsx
+++ src/pages/CurrentEmployees.jsx
@@ -1,8 +1,6 @@
 import DataTable from "datatable_react_nicolay";
-
 import * as employeeActions from "../store/storeSlice";
 import { useSelector } from "react-redux";
-// import "datatable_react_nicolay/dist/cjs/styles/style.css";
 import "../styles/Employee.css";
 
 function CreateEmployees() {
@@ -28,6 +26,245 @@ function CreateEmployees() {
     { label: "Department", id: "department", value: true, sortable: true, type:"string" },
   ];
 
+
+  // data test
+  // const dataPersist = [
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NJ",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2023",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "120 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2023",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "120 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2023",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "120 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "Jane",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "Washington",
+  //     state: "NY",
+  //     zipCode: "10001",
+
+  //     department: "IT",
+  //   },
+  //   {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     birthDate: "01/01/1990",
+  //     startDate: "01/01/2020",
+  //     streetAddress: "123 Main St",
+  //     city: "New York",
+  //     state: "NY",
+  //     zipCode: "10001",
+  //     department: "IT",
+  //   }
+  // ];
+
   return (
     <div className="container">
       <h1>Current Employees</h1>
@@ -44,6 +281,7 @@ function CreateEmployees() {
           data={data}
           language={true}
           pagination={true}
+          arialabels={true} //for screen readers
         />
       </section>
     </div>
