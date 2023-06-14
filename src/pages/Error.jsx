e368f1b494c60beb941f1bf0987e125b43e680a1
jucroizer <81417866+jucroizer@users.noreply.github.com>
2023-06-14 17:28:56 +0200

    page error 404

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+import "../styles/Error.css";
+
 function Error(){
     return (
-        <div>
-            <h1>404</h1>
-            <h2>Page Not Found</h2>
+        <div className="container-error">
+          <Link to="/" className="link-error">Oups! Revenez à la page d'accueil</Link>
         </div>
     )
 }
